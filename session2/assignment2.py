5c09cc38dfa2352aa365c5bb66f60e0482b0def4
Louis <gautamalouis30@gmail.com>
2022-09-17 15:30:50 +0800

    session 2 assignment

--- session2/assignment2.py
+++ session2/assignment2.py
@@ -3,9 +3,9 @@ import cv2
 cap = cv2.VideoCapture('Resources/elon.mp4')
 while True:
    success, img = cap.read()
-   img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-   cv2.imshow('Output',img_gray)
+   crop_img = img[100:300, 200:500]
+   cv2.imshow('Output',crop_img)
 
    if cv2.waitKey(1) & 0xFF == ord('q'):
        break
-    
+
