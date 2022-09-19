c50c6fda6dc58f49361b12eff8a6fffd331692ad
Louis <gautamalouis30@gmail.com>
2022-09-19 22:55:32 +0800

    assignment session 3

--- session3/assignment_session3/assignment_02_4.py
+++ session3/assignment_session3/assignment_02_4.py
@@ -1,14 +1,16 @@
 import cv2
-import numpy as np#Joining Image
+import numpy as np
+
 
 img1 = cv2.imread("Resources/lambo.png")
-crop_img1 = img1[0:200, 200:500]
-
 img2 = cv2.imread("Resources/robot.png")
-crop_img2 = img2[0:200, 200:500]
 
-img_hor = np.hstack((crop_img1,crop_img2)) #join pictures horizontally
-img_ver = np.vstack((crop_img1,crop_img2)) #join pictures vertically
+resized_img1 = cv2.resize(img1,(300,300))
+resized_img2 = cv2.resize(img2,(300,300))
+img_hor = np.hstack((resized_img1,resized_img2)) #join pictures horizontally
+img_ver = np.vstack((resized_img1,resized_img2)) #join pictures vertically
+
+
 
 cv2.imshow("Horizontal", img_hor)
 cv2.imshow("Vertical", img_ver)
