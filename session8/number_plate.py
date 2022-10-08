21a0c280b142f64eadaeac7dcad3e25681ed7306
Louis <gautamalouis30@gmail.com>
2022-10-08 14:10:58 +0700

    session8

--- session8/number_plate.py
+++ session8/number_plate.py
@@ -21,8 +21,8 @@ while True:
     for (x,y,w,h) in plates:
         area = w * h
         if area> min_area:
-           cv2.rectangle(img,(x,y), (x+w,y+h), (0,255,0), 2)
-           cv2.putText(img, "Number Plate", (x,y+5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,0,255), 2)
+           cv2.rectangle(img,(x,y-5), (x+w,y+h), (0,255,0), 2)
+           cv2.putText(img, "Number Plate", (x,y-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,0,255), 2)
            img_roi = img[y: y+h, x:x+w]
            cv2.imshow("ROI", img_roi)
 
